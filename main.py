1b83f1e7abe9da4860fb74dd438a86d0c423fe20
eBvel <belyakov041098@gmail.com>
2026-05-31 11:50:07 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_current_weather(city: str):
+    response = requests.get(
+        url=f'https://wttr.in/{city}?format=%t'
+    )
+    return f'{city}: {response.text}'
+
+if __name__ == '__main__':
+    city = 'Москва'
+    current_weather = get_current_weather(city)
+    print(current_weather)
